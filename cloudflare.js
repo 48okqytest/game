3a20382aad38a7e55dc3e0dbdfad32005c7a6fa4
48okqytest <forgorilla@yahoo.com>
2025-06-28 18:03:16 -0500

    Create cloudflare.js

---
+++ cloudflare.js
@@ -0,0 +1,17 @@
+{
+  "build": {
+    "command": "npm run build",
+    "publish": "public",
+    "nodeVersion": "16"
+  },
+  "routes": [
+    {
+      "handle": "filesystem"
+    },
+    {
+      "src": "/.*",
+      "dest": "/index.html",
+      "status": 200
+    }
+  ]
+}
